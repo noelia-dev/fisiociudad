4f8a0d5dcac84605590fcc1098a4feb372f497e9
noelia <noeliacortijo@gmail.com>
2023-05-08 13:12:24 +0200

    creación bbdd

--- script_bbdd/bbdd_creation.sql
+++ script_bbdd/bbdd_creation.sql
@@ -1,5 +1,2 @@
- show databases;
- 
- USE cake_cms;
- 
- CREATE DATABASE `app_nco` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE `app_nco` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE cake_cms;
