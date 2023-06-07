462b902714246c05cf938ace97c22a25d89ceeaa
noelia <noeliacortijo@gmail.com>
2023-06-07 23:14:24 +0200

    botón administrar

--- proyecto_cakephp/plugins/FrontTheme/templates/element/layout/topbar.php
+++ proyecto_cakephp/plugins/FrontTheme/templates/element/layout/topbar.php
@@ -8,7 +8,7 @@
                 <?= $this->Html->link(
                     $this->Html->image('logo_web.jpg', ['alt' => 'Fisiociudad', 'class' => 'justify-content-center']),
                     [
-                        'controller' => 'Admin', 'action' => '/usuarios/login'
+                        'action' => 'index'
                     ],
                     ['class' => 'd-none d-sm-inline-block btn btn-sm shadow-sm', 'escape' => false]
                 ); ?>
@@ -16,6 +16,16 @@
             <div class="col text-center d-flex align-items-center">
                 <h2 class="heading-section">Calendario de disponibilidad</h2>
             </div>
+            <div class="col text-center d-flex align-items-center">
+                <?= $this->Html->link(
+                    'Administrar',
+                    [
+                        'controller' => 'Admin', 'action' => '/usuarios/login'
+                    ],
+                    ['class' => 'd-none d-sm-inline-block btn btn-sm btn-primary shadow-sm']
+                ); ?>
+            </div>
         </div>
     </div>
+
 </nav>
