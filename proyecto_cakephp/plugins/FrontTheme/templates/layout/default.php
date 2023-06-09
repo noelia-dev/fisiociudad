2b2e5f46c9a17fb96822e81bf0d6c90109514689
noelia <noeliacortijo@gmail.com>
2023-06-09 14:29:26 +0200

    TITLE

--- proyecto_cakephp/plugins/FrontTheme/templates/layout/default.php
+++ proyecto_cakephp/plugins/FrontTheme/templates/layout/default.php
@@ -2,7 +2,6 @@
 <html lang="es">
 
 <head>
-    <title>FISIOCIUDAD - Administración de las citas</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
