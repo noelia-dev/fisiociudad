cfcdf4d621fe48a5dfa4bb20dcee37654c99fb6f
noelia <noe.cd@icloud.com>
2023-06-11 23:56:49 +0200

    footer mejor

--- proyecto_cakephp/plugins/BackTheme/templates/element/layout/footer.php
+++ proyecto_cakephp/plugins/BackTheme/templates/element/layout/footer.php
@@ -1,5 +1,6 @@
 <footer class="sticky-footer bg-white">
   <div class="container my-auto">
+  <p>Contacto: <strong>Noelia Cortijo Durán</strong> | Teléfono: <a href="tel:+34679663692">+34679663692</a> | Ubicación: Avenida Molinero, 44, Cañamero, Cáceres</p>
     <div class="copyright text-center my-auto">
       <span>Copyright &copy; FisioCiudad 2023</span>
     </div>
