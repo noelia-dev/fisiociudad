85fb5dddde9f5c5e1b63d733ad9e04f79ed4be46
noelia <noe.cd@icloud.com>
2023-06-11 23:58:17 +0200

    footer mejor

--- proyecto_cakephp/plugins/BackTheme/templates/element/layout/footer.php
+++ proyecto_cakephp/plugins/BackTheme/templates/element/layout/footer.php
@@ -1,4 +1,4 @@
-<footer class="sticky-footer bg-white">
+<footer class="bg-white">
   <div class="container my-auto">
   <p>Contacto: <strong>Noelia Cortijo Durán</strong> | Teléfono: <a href="tel:+34679663692">+34679663692</a> | Ubicación: Avenida Molinero, 44, Cañamero, Cáceres</p>
     <div class="copyright text-center my-auto">
